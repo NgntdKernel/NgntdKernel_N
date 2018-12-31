f73c22ee89b80f041875359fa86f7b9a308a391d
Ahmad Thoriq Najahi <tlogitechnjhi@gmail.com>
2019-01-01 00:58:30 +0700

    Merge tag 'v3.18.41' into cm-14.1
    Linux 3.18.41
    
    Signed-off-by: Ahmad Thoriq Najahi <tlogitechnjhi@gmail.com>

--- drivers/usb/host/xhci-ring.c
+++ drivers/usb/host/xhci-ring.c
@@ -1341,6 +1341,13 @@ static void handle_cmd_completion(struct xhci_hcd *xhci,
 		xhci_handle_stopped_cmd_ring(xhci, cmd);
 		return;
 	}
+
+	if (cmd->command_trb != xhci->cmd_ring->dequeue) {
+		xhci_err(xhci,
+			 "Command completion event does not match command\n");
+		return;
+	}
+
 	/*
 	 * Host aborted the command ring, check if the current command was
 	 * supposed to be aborted, otherwise continue normally.
